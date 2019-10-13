56911135a22b53c2de3769da2d0d947037b2d39f
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-13 17:20:39 +0200

    Module 5 Working with Array Keys and Composite Keys

--- CourseLibrary.API/Controllers/AuthorCollectionsController.cs
+++ CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,31 @@ namespace CourseLibrary.API.Controllers
                 throw new ArgumentNullException(nameof(mapper));
         }
 
+        // array key: 1,2,3,...
+        // composite key: key1=value1, key2=value2,...
+
+        [HttpGet("({ids})", Name = "GetAuthorCollection")]
+        public IActionResult GetAuthorCollection(
+            [FromRoute] 
+        [ModelBinder(BinderType =typeof(ArrayModelBinding))] IEnumerable<Guid> ids)
+        {
+            if(ids == null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+
+            return Ok(authorsToReturn);
+        }
+
         [HttpPost]
         public ActionResult<IEnumerable<AuthorDto>> CreatAuthorCollection (
             IEnumerable<AuthorForCreationDto> authorCollection)
@@ -37,7 +63,12 @@ namespace CourseLibrary.API.Controllers
 
             _courseLibraryRepository.Save();
 
-            return Ok();
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(x => x.Id));
+
+            return CreatedAtRoute("GetAuthorCollection",
+                new { ids = idsAsString },
+                authorCollectionToReturn);
         }
     }
 }
