3e357b931f336699bb2c6aee9bd20298620323b3
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-15 10:41:32 +0200

    Module 3 Data Shaping Single Resources

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -71,7 +71,7 @@ namespace CourseLibrary.API.Controllers
         }
 
         [HttpGet("{authorId}", Name = "GetAuthor")]
-        public IActionResult GetAuthor(Guid authorId)
+        public IActionResult GetAuthor(Guid authorId, string fields)
         {
             var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
 
@@ -79,7 +79,7 @@ namespace CourseLibrary.API.Controllers
             {
                 return NotFound();
             }
-            return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+            return Ok(_mapper.Map<AuthorDto>(authorFromRepo).ShapeData(fields));
         }
 
         [HttpPost]
---
+++ CourseLibrary.API/Helpers/ObjectExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Helpers
+{
+    public static class ObjectExtensions
+    {
+        public static ExpandoObject ShapeData<TSource>(this TSource source,
+            string fields)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            var dataShapedObject = new ExpandoObject();
+
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                // all public properties should be in the ExpandoObject
+                var propertyInfos = typeof(TSource)
+                    .GetProperties(BindingFlags.IgnoreCase |
+                    BindingFlags.Public | BindingFlags.Instance);
+
+                foreach (var propertyInfo in propertyInfos)
+                {
+                    // get the value of the property on the source object
+                    var propertyValue = propertyInfo.GetValue(source);
+
+                    // add field to the ExpandoObject, casted to IDictionary<K, V>
+                    ((IDictionary<string, object>)dataShapedObject)
+                        .Add(propertyInfo.Name, propertyValue);
+                }
+
+                return dataShapedObject;
+            }
+                
+                // the fields are separated by a ",", so we split it
+                var fieldsAfterSplit = fields.Split(',');
+
+                foreach (var field in fieldsAfterSplit)
+                {
+                    var propertyName = field.Trim();
+
+                    var propertyInfo = typeof(TSource)
+                        .GetProperty(propertyName, BindingFlags.IgnoreCase |
+                        BindingFlags.Public | BindingFlags.Instance);
+
+                    if (propertyInfo == null)
+                    {
+                        throw new Exception($"Property {propertyName} wasn't found on " +
+                            $"{typeof(TSource)}");
+                    }
+
+                    var propertyValue = propertyInfo.GetValue(source);
+
+                    // add the filed to the ExpandoObject
+                    ((IDictionary<string, object>)dataShapedObject)
+                        .Add(propertyInfo.Name, propertyValue);
+                }
+            // RETURN THE EXPANDOOBJECT
+            return dataShapedObject;
+        }
+
+    }
+}
